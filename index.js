99bd1a15362c202f9d98bac3858cb31882931029
Gourav Khunger <gouravkhunger18@gmail.com>
2022-10-23 02:32:02 +0530

    fix: handle undefined path requests

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var express = require('express');
-var app = express();
-var PORT = 3000;
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-var links = require('./links.json');
+const links = require('./links.json');
 
 app.get("/:link", (req, res) => {
-  res.status(301).redirect(links[req.params.link]);
+  const redirect = links[req.params.link];
+
+  if (redirect === null || redirect === undefined) {
+    res.status(400).json({"message": "Redirect does not exist!"});
+    return;
+  }
+
+  res.status(301).redirect(redirect);
 });
 
 app.get('/', (req, res) => {
